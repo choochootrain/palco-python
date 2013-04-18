6cd41bf905c5caee34db15bb2f6a6c2ec8af9218
Hurshal Patel <hpatel516@gmail.com>
2013-04-17 18:20:39 -0700

    added arrow and tweaked args

--- runner.py
+++ runner.py
@@ -3,16 +3,21 @@
 from utils import camera, bash
 import time
 import os
+import sys
 import readline
 
 cam = camera.Camera()
 shell = bash.Bash()
 
-while True:
-  cmd = raw_input()
+if len(sys.argv) == 1:
+  while True:
+    cmd = raw_input()
 
-  if cmd == 'cam':
+    if cmd == 'cam':
+      cam.step()
+      print cam.save()
+    elif cmd == 'programs':
+      print shell.run(os.getcwd() + '/utils/./times.sh')
+else:
+  while True:
     cam.step()
-    print cam.save()
-  elif cmd == 'programs':
-    print shell.run(os.getcwd() + '/utils/./times.sh')
--- utils/camera.py
+++ utils/camera.py
@@ -3,21 +3,15 @@
 import cv
 import datetime
 import os
+import math
 from random import Random
 
 class Camera:
   HAAR_CASCADE_PATH = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
   CAMERA_INDEX = 0
-  RANDOM = Random()
-  PT1 =  (RANDOM.randrange(-50, 2 * 50),
-      RANDOM.randrange(-50, 2 * 50))
-  line_type = cv.CV_AA
-  FONT = cv.InitFont(RANDOM.randrange(0, 8),
-    RANDOM.randrange(0, 100) * 0.05 + 0.01,
-    RANDOM.randrange(0, 100) * 0.05 + 0.01,
-    RANDOM.randrange(0, 5) * 0.1,
-    RANDOM.randrange(0, 10),
-    line_type)
+  LINE_TYPE = cv.CV_AA
+  FONT = cv.InitFont(0, 1, 1, 0, 3, LINE_TYPE)
+  WHITE = cv.Scalar(55, 255, 55)
 
   def __init__(self):
     cv.NamedWindow("w1", cv.CV_WINDOW_AUTOSIZE)
@@ -42,8 +36,8 @@ class Camera:
     print self.faces
 
     for (x,y,w,h) in self.faces:
-      cv.Rectangle(self.image, (x,y), (x+w,y+h), 255)
-      cv.PutText(self.image, "THIS IDIOT IS STILL ON THE COMPUTER", Camera.PT1, Camera.FONT, self.random_color(Camera.RANDOM))
+      cv.PutText(self.image, "THIS IDIOT IS STILL ON THE COMPUTER", (0, 30), Camera.FONT, Camera.WHITE)
+      arrow(self.image, (70, 40), (x, y), Camera.WHITE)
 
     cv.ShowImage("w1", self.image)
     cv.WaitKey(1)
@@ -55,6 +49,13 @@ class Camera:
     cv.SaveImage(name, self.image)
     return name
 
-  def random_color(self, random):
-    icolor = random.randint(0, 0xFFFFFF)
-    return cv.Scalar(icolor & 0xff, (icolor >> 8) & 0xff, (icolor >> 16) & 0xff)
+def arrow(img, p1, p2, color):
+  angle = math.atan(float(p2[1]-p1[1])/(p2[0]-p1[0]))
+  p2a = (p2[0]-int(10*math.cos(angle+math.pi/4)),
+         p2[1]-int(10*math.sin(angle+math.pi/4)))
+  p2b = (p2[0]-int(10*math.cos(angle-math.pi/4)),
+         p2[1]-int(10*math.sin(angle-math.pi/4)))
+
+  cv.Line(img, p1, p2,  color, 2)
+  cv.Line(img, p2a, p2, color, 2)
+  cv.Line(img, p2b, p2, color, 2)
