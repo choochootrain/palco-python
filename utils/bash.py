8642f12a0acb125e6a9ed659b9b4903de7da956e
Hurshal Patel <hpatel516@gmail.com>
2013-05-09 11:32:18 -0700

    add fork function

--- utils/bash.py
+++ utils/bash.py
@@ -9,3 +9,6 @@ class Bash:
     p = subprocess.Popen(['/bin/bash', '-c', cmd], stdout=subprocess.PIPE)
     out, err = p.communicate()
     return out
+
+  def fork(self, cmd):
+    p = subprocess.Popen(['/bin/bash', '-c', cmd], stdin=None, stdout=None, stderr=None, close_fds=True)
