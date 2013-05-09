8049f7d1327447a5b9680fe5492eca44145ad22d
Hurshal Patel <hpatel516@gmail.com>
2013-05-09 11:32:53 -0700

    added crappy fsm for randomized messages

--- runner.py
+++ runner.py
@@ -12,14 +12,25 @@ import serial
 cam = camera.Camera()
 shell = bash.Bash()
 events = events.Events()
+present = False
+responded = False
+
+welcomes = ['lazybones', 'alive', 'buddy']
+insults = ['event', 'facebook', 'homework', 'notpopular']
+
+def rand(arry):
+  return arry[int(random.random()*len(arry))]
+
+def say(msg):
+  return shell.run(os.getcwd() + '/utils/./say.sh ' + msg)
 
 if len(sys.argv) == 1:
   while True:
     cmd = raw_input()
+    cam.step(False)
     if cmd == 'quit':
       exit()
     elif cmd == 'cam':
-      cam.step(False)
       print cam.save()
     elif cmd == 'programs':
       print shell.run(os.getcwd() + '/utils/./times.sh')
@@ -28,7 +39,23 @@ if len(sys.argv) == 1:
       print evts[random.randint(0,len(evts)-1)]
     elif 'say' in cmd:
       cmds = cmd.split(' ')
-      print shell.run(os.getcwd() + '/utils/./say.sh ' + cmds[1])
+      say(cmds[1])
 else:
   while True:
     cam.step(True)
+    present = len(cam.face()) > 0
+
+    print present
+    if present and not responded:
+      say(rand(welcomes))
+      responded = True
+
+    if not present:
+      responded = False
+
+    if present:
+      if int(time.time()) % 8 == 0:
+        progs = shell.run(os.getcwd() + '/utils/./times.sh')
+
+        if 'chrome' in progs and random.random() > 0.95:
+          say(rand(insults))
