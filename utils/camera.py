c0046fc57104a8f1276e86033634051dc938d8e7
Hurshal Patel <hpatel516@gmail.com>
2013-05-09 11:09:00 -0700

    changed camera options and added new display

--- utils/camera.py
+++ utils/camera.py
@@ -4,14 +4,18 @@ import cv
 import datetime
 import os
 import math
+import time
 from random import Random
 
 class Camera:
   HAAR_CASCADE_PATH = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
-  CAMERA_INDEX = 1
+  EXTERNAL = False
+  CAMERA_INDEX = 1 if EXTERNAL else 0
   LINE_TYPE = cv.CV_AA
   FONT = cv.InitFont(0, 1, 1, 0, 3, LINE_TYPE)
-  WHITE = cv.Scalar(55, 255, 0)
+  WHITE = cv.Scalar(255, 255, 255)
+  GREEN = cv.Scalar(55, 255, 0)
+  RED = cv.Scalar(0, 0, 255)
 
   def __init__(self):
     cv.NamedWindow("w1", cv.CV_WINDOW_AUTOSIZE)
@@ -31,14 +35,24 @@ class Camera:
 
   def step(self, show):
     r_image = cv.QueryFrame(self.capture)
-    self.image = cv.CreateImage((r_image.height, r_image.width), r_image.depth, r_image.channels)
-    cv.Transpose(r_image, self.image)
-    cv.Flip(self.image, self.image, flipMode=0)
+
+    if Camera.EXTERNAL:
+      self.image = cv.CreateImage((r_image.height, r_image.width), r_image.depth, r_image.channels)
+      cv.Transpose(r_image, self.image)
+      cv.Flip(self.image, self.image, flipMode=0)
+    else:
+      self.image = r_image
+
 
     self.detect_faces()
 
     for (x,y,w,h) in self.faces:
-      cv.PutText(self.image, "LOOK AT THIS IDIOT", (0, 30), Camera.FONT, Camera.WHITE)
+      cv.PutText(self.image, "LOOK AT THIS IDIOT", (0, 30), Camera.FONT, Camera.RED)
+      cv.PutText(self.image, str(time.time()), (self.image.width - 275, self.image.height - 20), Camera.FONT, Camera.RED)
+      cv.Line(self.image, (0, y+h/2), (self.image.width, y+h/2), Camera.RED)
+      cv.Line(self.image, (x+w/2, 0), (x+w/2, self.image.height), Camera.RED)
+      cv.Circle(self.image, (x+w/2, y+h/2), min(h/2, w/2), Camera.RED, thickness=2)
+      cv.Circle(self.image, (x+w/2, y+h/2), min(h/8, w/8), Camera.RED)
       #arrow(self.image, (200, 40), (x, y), Camera.WHITE)
 
     if show:
